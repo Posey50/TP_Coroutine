f28ae3264f16f9eba38cc2550d940c42cc403e38
Posey50 <leolecointre50@gmail.com>
2024-02-01 19:47:48 +0100

    Fin du TP

---
+++ Assets/Scripts/AsyncScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AsyncScript : MonoBehaviour
+{
+    /// <summary>
+    /// Speed of the rotation.
+    /// </summary>
+    [SerializeField]
+    private float _rotationSpeed;
+
+    /// <summary>
+    /// Button to click to stop the rotation.
+    /// </summary>
+    [SerializeField]
+    private Button _cancelButton;
+
+    /// <summary>
+    /// Value indicating that the cube has to rotate.
+    /// </summary>
+    private bool _isRotating;
+
+    public void StartRotation()
+    {
+        if (!_isRotating)
+        {
+            Rotate();
+        }
+    }
+
+    /// <summary>
+    /// Waits 5 seconds before to stop the rotation, or even before if the cancel button is pressed.
+    /// </summary>
+    /// <returns></returns>
+    private async UniTask Rotate()
+    {
+        CancellationTokenSource tokenSource = new ();
+
+        _cancelButton.onClick.AddListener(() =>
+        {
+            tokenSource.Cancel();
+        });
+
+        _isRotating = true;
+        await UniTask.Delay(5000, cancellationToken: tokenSource.Token).SuppressCancellationThrow();
+        _isRotating = false;
+    }
+
+    /// <summary>
+    /// Makes the cube rotate while the bool is true.
+    /// </summary>
+    private void FixedUpdate()
+    {
+        if (_isRotating)
+        {
+            Vector3 rotation = new(0f, _rotationSpeed * Time.deltaTime, 0f);
+            transform.Rotate(rotation, Space.World);
+        }
+    }
+}
---
+++ Assets/Scripts/CoroutineScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoroutineScript : MonoBehaviour
+{
+    /// <summary>
+    /// Coroutine of that make the cube rotate.
+    /// </summary>
+    private Coroutine _rotationRoutine;
+
+    /// <summary>
+    /// Speed of the rotation.
+    /// </summary>
+    [SerializeField]
+    private float _rotationSpeed;
+
+    /// <summary>
+    /// Called to start the rotation.
+    /// </summary>
+    public void StartRotation()
+    {
+        if (_rotationRoutine == null)
+        {
+            _rotationRoutine = StartCoroutine(Rotation());
+        }
+    }
+
+    /// <summary>
+    /// Called to stop the rotation.
+    /// </summary>
+    public void StopRotation()
+    {
+        if (_rotationRoutine != null)
+        {
+            StopCoroutine(_rotationRoutine);
+            _rotationRoutine = null;
+        }
+    }
+
+    /// <summary>
+    /// Waits 5 seconds before to stop the rotation.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Rotation()
+    {
+        yield return new WaitForSeconds(5f);
+        _rotationRoutine = null;
+    }
+
+    /// <summary>
+    /// Makes the cube rotate while the coroutine is in progress.
+    /// </summary>
+    private void FixedUpdate()
+    {
+        if ( _rotationRoutine != null )
+        {
+            Vector3 rotation = new (0f, _rotationSpeed * Time.deltaTime, 0f);
+            transform.Rotate(rotation, Space.World);
+        }
+    }
+}
